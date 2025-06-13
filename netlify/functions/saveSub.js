dbce67149e36f5639f62cf210174495e7b80fbab
SanAppsMano <manosandeslimafilho@gmail.com>
2025-06-13 08:22:51 -0300

    Update saveSub.js

--- netlify/functions/saveSub.js
+++ netlify/functions/saveSub.js
@@ -2,17 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
+  if (event.httpMethod !== 'POST') return { statusCode: 405, body: 'Method Not Allowed' };
+
   const sub = JSON.parse(event.body);
-  const file = path.resolve(__dirname, 'subs.json');
-  let subs = [];
-  if (fs.existsSync(file)) {
-    subs = JSON.parse(fs.readFileSync(file));
-  }
-  // evita duplicatas
+  const filePath = path.join(__dirname, 'subs.json');
+  const subs = fs.existsSync(filePath)
+    ? JSON.parse(fs.readFileSync(filePath))
+    : [];
+
   if (!subs.find(s => s.endpoint === sub.endpoint)) subs.push(sub);
-  fs.writeFileSync(file, JSON.stringify(subs, null, 2));
+  fs.writeFileSync(filePath, JSON.stringify(subs, null, 2));
+
   return { statusCode: 200, body: 'Subscription saved.' };
 };
